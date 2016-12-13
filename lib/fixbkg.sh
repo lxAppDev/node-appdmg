e77454c28a57bfc38fb3681a5eb5de228a5ec2f5
lxAppDev <lxappdev@gmail.com>
2016-12-13 12:56:55 +0100

    added window x, y handling

--- lib/fixbkg.sh
+++ lib/fixbkg.sh
@@ -4,13 +4,13 @@ echo '
            open
            set toolbar visible of container window to false
            set statusbar visible of container window to false
-           set the bounds of container window to {0, 0, '$2', '$3'}
+           set the bounds of container window to {'$2', '$3', '$4', '$5'}
            set theViewOptions to the icon view options of container window
            set arrangement of theViewOptions to not arranged
-           set icon size of theViewOptions to '$4'
-           set background picture of theViewOptions to file ".background:'$5'"
-           set position of item "'$6'" of container window to {'$7', '$8'}
-           set position of item "'$9'" of container window to {'${10}', '${11}'}
+           set icon size of theViewOptions to '$6'
+           set background picture of theViewOptions to file ".background:'$7'"
+           set position of item "'$8'" of container window to {'$9', '${10}'}
+           set position of item "'${11}'" of container window to {'${12}', '${13}'}
            update without registering applications
            delay 5
            close
